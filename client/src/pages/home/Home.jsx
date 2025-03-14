61036d1ae9b879885f19c66a5348cae24d146f8f
Moiz Uddin M. Abdullah <moiz211j@gmail.com>
2025-03-14 16:48:59 +0500

    Basic setup complete

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import "./home.css"
+import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
 function home() {
   return (
     <div>
-      Home
+      <Navbar/>
+      <Header/>
     </div>
   )
 }
---
+++ client/src/pages/hotel/Hotel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function hotel() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default hotel
